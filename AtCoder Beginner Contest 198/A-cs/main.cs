d8ba5400b415a4a698a6fc9a80c8abd135bc9679
MAIGOMAIGO <63392807+MAIGOMAIGO@users.noreply.github.com>
2022-01-09 00:37:31 +0900

    Create main.cs

---
+++ AtCoder Beginner Contest 198/A-cs/main.cs
@@ -0,0 +1,9 @@
+public static class main
+{ 
+  public static void Main(string[] args)
+  {
+    string line = System.Console.ReadLine();
+    int N = System.Int32.Parse(line);
+    System.Console.WriteLine(N-1);
+  }
+}
