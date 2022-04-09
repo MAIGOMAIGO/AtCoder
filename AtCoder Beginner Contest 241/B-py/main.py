243dd2840428f1625dbc409da8cd3a659e4c3513
maigo <63392807+MAIGOMAIGO@users.noreply.github.com>
2022-04-10 00:31:01 +0900

    Create main.py

---
+++ AtCoder Beginner Contest 241/B-py/main.py
@@ -0,0 +1,10 @@
+N,M = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+for i in range(M):
+  if B[i] in A:
+    A.remove(B[i])
+  else:
+    print("No")
+    exit(0)
+print("Yes")
