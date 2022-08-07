598b4d0bfb8abc0ac87a4bf024578317c6b40329
maigo <63392807+MAIGOMAIGO@users.noreply.github.com>
2022-08-08 00:15:36 +0900

    Create Main.java

---
+++ AtCoder Beginner Contest 202/A-java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = scan.nextInt();
+    System.out.println(21-(a+b+c));
+  }
+}
