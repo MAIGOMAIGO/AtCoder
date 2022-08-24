8a44a2cf135985ee9545e04139cbda6cd0933189
maigo <63392807+MAIGOMAIGO@users.noreply.github.com>
2022-08-24 10:13:51 +0900

    Create Main.java

---
+++ AtCoder Beginner Contest 174/A-java/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int X = scan.nextInt();
+    System.out.println(X>=30?"Yes":"No");
+  }
+}
