c87efdab481d3d339157c41db00151d7eeacb5a3
maigo <63392807+MAIGOMAIGO@users.noreply.github.com>
2024-04-25 23:38:51 +0900

    Create main.py

---
+++ AtCoder Beginner Contest 344/C-py/main.py
@@ -0,0 +1,15 @@
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+L = int(input())
+C = list(map(int, input().split()))
+Q = int(input())
+X = list(map(int, input().split()))
+S = set()
+for i in A:
+  for j in B:
+    for k in C:
+      S.add(i+j+k)
+for i in X:
+  print("Yes" if i in S else "No")
